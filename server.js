d997b8b022612bf22747f7b47e6939168ccf8e62
axstutoring <kevintang01@ucla.edu>
2023-04-22 20:24:10 -0700

    Added authentication

--- server.js
+++ server.js
@@ -26,8 +26,23 @@ connectDB().then(() => {
     app.listen(8080, () => {console.log("Server listening on port 8080");});
 })
 
-app.post('/post', async (req, res) => {
+const User = require("./models/users");
 
+app.get('/auth', async (req, res) => {
+    let flag = "";
+    const feed = await User.find();
+    for (let i = 0; i < feed.length; i++)
+    {
+        if (feed[i].name === req.query.user)
+        {
+            if (crypto.SHA256(req.query.pass).toString() === feed[i].password)
+            {
+                flag = feed[i]._id;
+                break;
+            }
+        }
+    }
+    res.json(flag);
 })
 
 //const app = express();
@@ -35,9 +50,15 @@ app.post('/post', async (req, res) => {
 // Define your API endpoints here
 app.get('/api/cohere', async (req, res) => {
   // Call the cohere.generate function
+
+  let message = req.query.input;
+  if (req.query.input[req.query.input.length - 1] === ' ')
+  {
+    message = req.query.input.substring(0, req.query.input.length - 1);
+  }
   const response = await cohere.generate({
     model: 'command-xlarge-nightly',
-    prompt: req.query.input,
+    prompt: message,
     max_tokens: 300,
     temperature: 1,
     k: 0,
