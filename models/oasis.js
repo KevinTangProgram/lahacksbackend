1985ea573a4a78f992ce2623a48bc933413eaab2
axstutoring <kevintang01@ucla.edu>
2023-05-13 22:13:02 -0700

    Wrote oasis Schema

--- models/oasis.js
+++ models/oasis.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const OasisSchema = new Schema ({
-    title: {
-        type: String,
+    info: {
+        type: Array,
         required: true
     },
     topic: {
--- models/user.js
+++ models/user.js
@@ -2,16 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema ({
-    name: {
-        type: String,
+    info: {
+        type: Array,
         required: true
     },
-    password: {
-        type: String,
+    settings: {
+        type: Object,
         required: true
     },
-    email: {
-        type: String,
+    stats: {
+        type: Array,
         required: true
     },
     oasis: {
