9ddec11f36f9aa65fa7e21878b4592a3e7a9bed0
axstutoring <kevintang01@ucla.edu>
2023-06-05 17:21:28 -0700

    Added prompt 2.1

--- zzserver.js
+++ zzserver.js
@@ -100,7 +100,7 @@ app.put('/oasis/promptx1', async (req, res) => {
     let processedResponse = [];
     let headers = [];
     let messageID = "";
-    for (let i = 0; i < response.data.choices[0].text.length; i++) 
+    for (let i = 0; i < response.data.choices[0].text.length - 4; i++) 
     {
         if (response.data.choices[0].text[i] === "[" && response.data.choices[0].text[i + 1] === "I" && 
         response.data.choices[0].text[i + 2] === "D" && response.data.choices[0].text[i + 3] === ":")
@@ -153,21 +153,43 @@ app.put('/oasis/promptx2', async (req, res) => {
     "Your Messages:\n";
     
     for (let i = 0; i < req.body.rawMessage.length; i++) {
-        message += "[ID:" + i.toString() + "] " + req.body.rawMessage[i].text + "\n";
+        message += "[ID:" + i.toString() + "] " + req.body.rawMessage[i] + "\n";
     }
 
     const openai = new OpenAIApi(configuration);
     const response = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: message,
-        max_tokens: 7,
+        max_tokens: 500,
         temperature: 0,
     });
+    // console.log(response.data.choices[0].text);
+
+    // let response = {data: {choices: [{text: "[ID:1H] Existing header: Acid-Base Equilibria\n" +
+    //     "[ID:header] Generated header: Henderson Hasselback Equation\n" +
+    //     "[ID:2] At 1 pH higher than the acid pKa, the ratio of HA to A- is 1:10.\n" +
+    //     "[ID:3] At the acid pKa, the ratio of HA to A- is 1:1.\n" +
+    //     "[ID:4] At 1 pH lower than the acid pKa, the ratio of HA to A- is 10:1.\n" +
+    //     "[ID:header] Generated header: pI\n" +
+    //     "[ID:5] The pI is the average of the two pKas around the neutral range when the molecule is a zwitterion.\n" +
+    //     "[ID:header] Generated header: Net Charge\n" +
+    //     "[ID:6] At the carboxy pKa, the net charge is closer to 0.5.\n" +
+    //     "[ID:7] At the amino pKa, the net charge is closer to -0.5.\n" +
+    //     "[ID:header] Generated header: Other pH Facts\n" +
+    //     "[ID:8] As the pH range decreases, the positive charge increases.\n" +
+    //     "[ID:9] The pH can sometimes vary in the body.\n" +
+    //     "[ID:header] Generated header: Amino Acids\n" +
+    //     "[ID:10] Amino acids have a basic and acidic end.\n" +
+    //     "[ID:11] They are the building blocks of proteins.\n" +
+    //     "[ID:header] Generated header: Substitution Reactions\n" +
+    //     "[ID:12] There are two types of substitution reactions: sn1 and sn2.\n" + 
+    //     "[ID:13] Sn1 reactions happen faster than sn2 reactions."}]}};
 
     let processedResponse = [];
     let headers = [];
+    let nonHeaders = [];
     let messageID = "";
-    for (let i = 0; i < response.data.choices[0].text.length; i++) 
+    for (let i = 0; i < response.data.choices[0].text.length - 4; i++) 
     {
         if (response.data.choices[0].text[i] === "[" && response.data.choices[0].text[i + 1] === "I" && 
         response.data.choices[0].text[i + 2] === "D" && response.data.choices[0].text[i + 3] === ":")
@@ -175,28 +197,50 @@ app.put('/oasis/promptx2', async (req, res) => {
             i+=4;
             while (response.data.choices[0].text[i] !== "]")
             {
-                if (response.data.choices[0].text[i] === "H")
-                {
-                    
-                }
                 messageID += response.data.choices[0].text[i];
                 i++;
             }
-            headers.push(Number(messageID));
+            i += 2;
+            let lineMessage = "";
+            for (; i < response.data.choices[0].text.length; i++)
+            {
+                if (response.data.choices[0].text[i + 1] === "[" && response.data.choices[0].text[i + 2] === "I" && 
+                response.data.choices[0].text[i + 3] === "D" && response.data.choices[0].text[i + 4] === ":")
+                {
+                    break;
+                }
+                else
+                {
+                    lineMessage += response.data.choices[0].text[i];
+                }
+            }
+            if (messageID.includes("H") || messageID.includes("header"))
+            {
+                lineMessage = lineMessage.replace("Generated header: ", "");
+                headers.push([i, lineMessage]);
+            }
+            else
+            {
+                nonHeaders.push([i, lineMessage]);
+            }
             messageID = "";
         }
     }
+    headers.push([response.data.choices[0].text.length + 1, ""])
     let j = 0;
-    for (let i = 0; i < req.body.rawMessage.length; i++)
+    for (let i = 0; i < headers.length; i++)
     {
-        if (j < headers.length && headers[j] === i)
+        for (; j < nonHeaders.length; j++)
         {
-            processedResponse.push({header: true, message: req.body.rawMessage[i]});
-            j++;
-        }
-        else
-        {
-            processedResponse.push({header: false, message: req.body.rawMessage[i]});
+            if (headers[i][0] < nonHeaders[j][0])
+            {
+                processedResponse.push({header: true, message: headers[i][1]});
+                break;
+            }
+            else
+            {
+                processedResponse.push({header: false, message: nonHeaders[j][1]});
+            }
         }
     }
     res.json(processedResponse);
